c3c6635fab1ac394c89f123c472ad75ad7b35dfb
numerico <numerico@HPMX>
2023-03-30 17:51:08 -0600

    1er test geometry

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,15 +4,9 @@ using Test
 @testset "geometry" begin
 
     @test 2 == 2
-    #v = [5.0;-1.0]
-    #@test J₂(v) == [v[2]; -v[1]]
-    #@test length(v) == length(J₂(v)) == 2
-    #@test_throws BoundsError J₂([1.0])
-    #@test_broken J₂(3)
-    # @time_imports
-
-end
-
-@testset "IO" begin
+    v = [5.0;-1.0]
+    @test J₂(v) == [v[2]; -v[1]]
+    @test length(v) == length(J₂(v)) == 2
+    @test_throws BoundsError J₂([1.0])
 
 end
