ef8de273810aa171cb956422c67c742f043b4027
Miguel Raz <miguelraz@ciencias.unam.mx>
2023-08-16 21:56:54 -0600

    delete bnd_dict and bnd_window

--- src/contour_creator/bnd_windict.jl
+++ src/contour_creator/bnd_windict.jl
@@ -1,23 +1,3 @@
-function bnd_window(fig::Figure, str::String)
-    return Axis(fig[1, 1],
-        xlabel=str,
-        xminorticksvisible=true,
-        yminorticksvisible=true,
-        xminorgridvisible=true,
-        yminorgridvisible=true,
-        #xminorticks=IntervalsBetween(11),
-        #yminorticks=IntervalsBetween(11),
-        xminorgridwidth=1,
-        yminorgridwidth=1,
-        xgridwidth=1,
-        ygridwidth=1,
-        xminorgridcolor="gray",
-        yminorgridcolor="gray",
-        xgridcolor="gray",
-        ygridcolor="gray"
-    )
-end
-
 function region_window(fig::Figure, str::String, n::Int64)
     ax = Axis(fig[1, 1:n],
         xlabel=str,
@@ -35,23 +15,4 @@ function region_window(fig::Figure, str::String, n::Int64)
     ax.aspect = DataAspect()
     hidespines!(ax)
     return ax
-end
-
-function bnd_dict(R::DataRegion)
-
-    nH = length(R.H)
-    list = [0]
-    Dpts = Dict{Int64,Observable{VecPts}}()
-    Dflpts = Dict{Int64,Observable{VecPts}}()
-    Dpts[0] = bnd2obs(R.E)
-    Dflpts[0] = @views bnd2obs(R.E[[1, end], :])
-    # fill dictionaries of the boundary points
-    if nH > 0
-        for i = 1:nH
-            push!(list, i)
-            Dpts[i] = bnd2obs(R.H[i])
-            Dflpts[i] = @views bnd2obs(R.H[i][[1, end], :])
-        end
-    end
-    return Dpts, Dflpts, list
 end
