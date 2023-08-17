4f1ecafa79147a0a2699ea2b4d28e35722c4c131
Miguel Raz <miguelraz@ciencias.unam.mx>
2023-08-17 10:23:14 -0600

    arreglar distseg

--- src/geometry/distseg.jl
+++ src/geometry/distseg.jl
@@ -1,24 +1,20 @@
 
-	"""
-	    distseg(a,b,p)
+"""
+    distseg(a,b,p)
 
-	Compute euclidean distance from the segment ab 
-	to the point p.
-	"""
-	function distseg(a::Union{Vector{Float32},Vector{Float64}}, 
-					 b::Union{Vector{Float32},Vector{Float64}}, 
-					 p::Union{Vector{Float32},Vector{Float64}}
-					 )
-
-		 	u = b-a
-	        v = p-a
-			c = (u'v)/(u'u)
-			if  c > 1
-		    	proy = b
-			else
-				proy = a
-		       	(0 ≤c ≤ 1) && (proy += c*u)
-		    end
-		    return norm(p-proy)
-	end
-distseg(C::Matrix{Float64}, i::Int64, p::Int64, q::Int64) = distseg(C[p,:],C[q,:],C[i,:])
+Compute euclidean distance from the segment ab 
+to the point p.
+"""
+function distseg(a::AbstractVector{T}, b::AbstractVector{T}, p::AbstractVector{T}) where T
+    u = b - a
+    v = p - a
+    c = (u'v) / (u'u)
+    if c > 1
+        proy = b
+    else
+        proy = a
+        (0 ≤ c ≤ 1) && (proy += c * u)
+    end
+    return norm(p - proy)
+end
+distseg(C::Matrix{Float64}, i::Int64, p::Int64, q::Int64) = @views distseg(C[p, :], C[q, :], C[i, :])
